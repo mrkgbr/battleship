c8d4b462865bcf448939a21e210d91a58175689c
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-05-29 11:51:41 +0200

    Add linter and formatter

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ const body = document.querySelector("body");
 const title = document.createElement("h1");
 title.textContent = "Battleship project";
 body.appendChild(title);
-console.log(myFunction("teszt"));
+console.log(myFunction("testing"));
